dca5fb97427d96ec7f38682b46299cfbd1d36bec
Mariano <mariano.ramaidana@gmail.com>
2025-06-09 11:10:31 -0300

    se añadió paquete input, para el ingreso de información por teclado

---
+++ src/main/java/com/informatorio/appligachad/input/IngresoPorTeclado.java
@@ -0,0 +1,30 @@
+package com.informatorio.appligachad.input;
+
+import java.util.Scanner;
+
+public class IngresoPorTeclado {
+	
+	static Scanner entrada;
+	
+	public static int ingresarEnteroPositivo() {
+		entrada = new Scanner(System.in);
+		int valor = -1;
+		while(valor<0) {
+			try {
+				valor = Integer.parseInt(entrada.nextLine());
+			}catch(NumberFormatException e) {
+				System.out.println("Entrada inválida. Ingrese un número entero.");
+			}
+		}
+		entrada.close();
+		return valor;
+	}
+	
+	public static String ingresarTexto() {
+		entrada = new Scanner(System.in);
+		String texto = entrada.nextLine();
+		entrada.close();
+		return texto;
+	}
+
+}
