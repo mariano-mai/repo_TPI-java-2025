5a499ad646f740e27240eface4eb38208e692e41
Mariano <mariano.ramaidana@gmail.com>
2025-06-09 00:02:39 -0300

    primer commit, inicio del proyecto

---
+++ src/main/java/com/informatorio/appligachad/App.java
@@ -0,0 +1,10 @@
+package com.informatorio.appligachad;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
