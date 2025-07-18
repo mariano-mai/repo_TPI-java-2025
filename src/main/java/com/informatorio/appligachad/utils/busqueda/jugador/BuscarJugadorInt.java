a1c1cdd9a502e3039d50322ebd0c753b74a7b52d
Mariano <mariano.ramaidana@gmail.com>
2025-07-18 02:16:56 -0300

    Se añadió un nuevo método de búsqueda de Jugador

--- src/main/java/com/informatorio/appligachad/utils/busqueda/jugador/BuscarJugadorInt.java
+++ src/main/java/com/informatorio/appligachad/utils/busqueda/jugador/BuscarJugadorInt.java
@@ -1,9 +1,12 @@
 package com.informatorio.appligachad.utils.busqueda.jugador;
 
+import com.informatorio.appligachad.domain.Equipo;
 import com.informatorio.appligachad.domain.Jugador;
 
 public interface BuscarJugadorInt {
 	
 	Jugador buscar();
+	
+	Jugador buscar(Equipo equipo);
 
 }
--- src/main/java/com/informatorio/appligachad/utils/busqueda/jugador/impl/BuscarJugadorIntImpl.java
+++ src/main/java/com/informatorio/appligachad/utils/busqueda/jugador/impl/BuscarJugadorIntImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.informatorio.appligachad.database.domaindb.JugadorDB;
+import com.informatorio.appligachad.domain.Equipo;
 import com.informatorio.appligachad.domain.Jugador;
 import com.informatorio.appligachad.input.IngresoPorTeclado;
 import com.informatorio.appligachad.utils.busqueda.jugador.BuscarJugadorInt;
@@ -11,6 +12,7 @@ import com.informatorio.appligachad.utils.busqueda.jugador.BuscarJugadorInt;
 public class BuscarJugadorIntImpl implements BuscarJugadorInt{
 	
 	Jugador nuevoJugador;
+	Map<Integer, Jugador> mapaTemporal;
 
 	@Override
 	public Jugador buscar() {
@@ -19,7 +21,7 @@ public class BuscarJugadorIntImpl implements BuscarJugadorInt{
 
 	
 	private Jugador buscarJugador(Map<String, Jugador> jugadores) {
-		Map<Integer, Jugador> mapaTemporal = new HashMap<>();
+		mapaTemporal = new HashMap<>();
 		System.out.println("\nSelección de Jugador");
 		System.out.println("Ingrese la primer letra del APELLIDO del jugador: ");
 		String letra = IngresoPorTeclado.ingresarTexto().toUpperCase();
@@ -40,6 +42,32 @@ public class BuscarJugadorIntImpl implements BuscarJugadorInt{
 		}else if(i==1) {
 			System.out.println("No se encontraron coincidencias.");
 			System.out.println("Pruebe con otra inicial o registre un jugador nuevo.\n");
+		}
+		return nuevoJugador;
+	}
+
+
+	@Override
+	public Jugador buscar(Equipo equipo) {
+		return buscarJugador(equipo);
+	}
+	
+	private Jugador buscarJugador(Equipo equipo) {
+		mapaTemporal = new HashMap<>();
+		int i = 1;
+		for(Jugador jugador1 : equipo.getJugadores()) {
+			mapaTemporal.put(i, jugador1);
+			i++;
+		}
+		if(i!=1) {
+			System.out.println("Seleccione el jugador: ");
+			for(Map.Entry<Integer, Jugador> jugador2 : mapaTemporal.entrySet()) {
+				System.out.println(jugador2.getKey()+"- "+jugador2.getValue().getNombre());
+			}
+			int valor = IngresoPorTeclado.ingresarEnteroPositivo();
+			nuevoJugador = mapaTemporal.get(valor);
+		}else if(i==1) {
+			System.out.println("Este equipo no tiene jugadores.");
 		}
 		return nuevoJugador;
 	}
